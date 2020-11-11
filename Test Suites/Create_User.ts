dc9db4701943c6debe1c78b37228c6c73d588593
Meet Patel <patelmeet8797@gmail.com>
2020-11-11 08:11:56 +0530

    Get List and Create User API's

---
+++ Test Suites/Create_User.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Create_User</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e7ae3b78-4a05-483b-8b46-c9b6d2010630</testSuiteGuid>
+   <testCaseLink>
+      <guid>d62f5710-4a3a-4514-97dc-4605433f4c85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PostTestCases</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Get_User.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Get_User</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>19c6e5f7-db31-4301-9827-2c4b96aad38e</testSuiteGuid>
+   <testCaseLink>
+      <guid>19060802-6147-48fd-bac8-ec080da9df40</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
